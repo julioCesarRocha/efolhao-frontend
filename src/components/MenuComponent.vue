5bc469c4e513c479cd097f0082c9cf89d02209d7
Julio Cesar <julio.junior1@estudante.ifb.edu.br>
2023-02-01 09:26:29 -0300

    Pré implementação do VueRouter

---
+++ src/components/MenuComponent.vue
@@ -0,0 +1,59 @@
+<template>
+    <v-container>
+      <v-row>
+        <v-col cols="12" md="6">
+          <v-card @click="redirectToForm1">
+            <v-card-title>Sinais Vitais</v-card-title>
+          </v-card>
+        </v-col>
+        <v-col cols="12" md="6">
+          <v-card @click="redirectToForm2">
+            <v-card-title>Hemodinâmica</v-card-title>
+          </v-card>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col cols="12" md="6">
+          <v-card @click="redirectToForm3">
+            <v-card-title>Respiração</v-card-title>
+          </v-card>
+        </v-col>
+        <v-col cols="12" md="6">
+          <v-card @click="redirectToForm4">
+            <v-card-title>Neurológico</v-card-title>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
+  </template>
+  
+<script>
+  export default {
+    methods: {
+      redirectToForm1() {
+        this.$router.push({ name: "SinaisVitais" });
+      },
+      redirectToForm2() {
+        this.$router.push({ name: "Hemodinamica" });
+      },
+      redirectToForm3() {
+        this.$router.push({ name: "Hemodinamica" });
+      },
+      redirectToForm4() {
+        this.$router.push({ name: "Neurologico" });
+      },
+    },
+  };
+</script>
+<style scoped>
+    .v-card {
+        display: flex;
+        justify-content: center;
+        height: 250px;
+        width: 400px;
+        background-color: #5faff0;
+        color: white;
+        /* flex-wrap: wrap; */
+
+    }
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,21 +1,55 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../views/Home.vue'
+// import { createRouter, createWebHistory } from 'vue-router'
+// import Home from '../views/Home.vue'
+
+// const routes = [
+//   {
+//     path: '/',
+//     name: 'Home',
+//     component: Home
+//   },
+//   {
+//     path: '/hemodinamica',
+//     name: 'Hemodinamica',
+//     component: () => import(/* webpackChunkName: "hemodinamica" */ '../views/HemodinamicaApp.vue')
+//   }
+// ]
+
+// const router = createRouter({
+//   history: createWebHistory(process.env.BASE_URL),
+//   routes
+// })
+
+// export default router
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import SinaisVitais from './components/SinaisVitais.vue'
+import Respiracao from './components/Respiracao.vue'
+import Hemodinamica from './components/Hemodinamica.vue'
+// import Form4 from './components/Form4.vue'
+
+Vue.use(VueRouter)
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
+    path: '/sinaisvitais',
+    name: 'SinaisVitais',
+    component: SinaisVitais
+  },
+  {
+    path: '/respiracao',
+    name: 'Respiracao',
+    component: Respiracao
   },
   {
     path: '/hemodinamica',
     name: 'Hemodinamica',
-    component: () => import(/* webpackChunkName: "hemodinamica" */ '../views/HemodinamicaApp.vue')
+    component: Hemodinamica
   }
 ]
 
-const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
   routes
 })
 
