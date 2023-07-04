605b2c4a0c75f88a5bf6db7c3ea30ced43b98caf
Julio Cesar <julio.junior1@estudante.ifb.edu.br>
2023-07-03 22:21:40 -0300

    Retornando versão correta da App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,45 @@
 <template>
   <v-app>
-    <NavbarApp :logo="logo_src" :alt="app_name"/>
-    <!-- <LoginComponent />  -->
-    <!-- <PacientesComponent />
-    <MenuComponentVue /> -->
-    <MenuComponentVue /> 
-    <!-- <NeurologicoComponent /> 
-    <UsuarioComponent />
-    <HemodinamicaApp />
-    <RespiracaoComponent />  -->
-      
+    <NavbarApp />
+    <MenuComponent />
+    <LoginComponent v-if="showLogin" /> 
+    <router-view v-else></router-view>
   </v-app>
 </template>
 
 <script>
-import MenuComponentVue from './components/MenuComponent.vue';
+import LoginComponent from "./components/LoginComponent.vue";
 import NavbarApp from "./components/Navbar.vue";
-// import PacientesComponent from "./components/PacientesComponent.vue";
-// import SinaisVitais from "./components/SinaisVitais.vue";
-// import HemodinamicaApp from "./components/Hemodinamica.vue";
-// import RespiracaoComponent from "./components/Respiracao.vue";
-// import LoginComponent from "./components/LoginComponent.vue";
-// import UsuarioComponent from "./components/UsuarioComponent.vue";
-// import NeurologicoComponent from "./components/NeurologicoComponent.vue";
+// import MenuComponent from "./components/MenuComponent.vue";
+
 
 export default {
   data: function() {
     return {
       logo_src: "/logo.png",
-      app_name: "e-Sinais"
+      app_name: "e-Sinais",
+      showLogin: true
     }
   },
   components: {
-    NavbarApp,
-    // UsuarioComponent,
-    // NeurologicoComponent,
-    // SinaisVitais,
-    // HemodinamicaApp,
-    // RespiracaoComponent,
-    // PacientesComponent,
-    // LoginComponent,
-    MenuComponentVue,
-},
-
+    LoginComponent,
+    // MenuComponent,
+    NavbarApp
+  },
+  created() {
+    this.$router.beforeEach((to, from, next) => {
+      // Verifica se a rota atual é diferente de http://localhost:8080/
+      if (to.fullPath !== '/') {
+        // Esconde o componente LoginComponent
+        this.showLogin = false;
+      } else {
+        // Mostra o componente LoginComponent
+        this.showLogin = true;
+      }
+      console.log(this.showLogin)
+      // Chama o próximo middleware ou a rota alvo
+      next();
+    })
+  }
 };
 </script>
